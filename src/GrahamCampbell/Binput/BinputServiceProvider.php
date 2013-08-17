bc0ba0cafef3bcd93b95e527ed50cb7a3f0f132c
Graham Campbell <graham@mineuk.com>
2013-08-17 14:46:08 +0100

    Updated the providers

--- src/GrahamCampbell/Binput/BinputServiceProvider.php
+++ src/GrahamCampbell/Binput/BinputServiceProvider.php
@@ -37,6 +37,6 @@ class BinputServiceProvider extends ServiceProvider {
      * @return array
      */
     public function provides() {
-        return array();
+        return array('binput');
     }
 }
