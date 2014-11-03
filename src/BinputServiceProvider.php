aa6867712d24cb837a997aa39526d5f0f452fece
Graham Campbell <graham@mineuk.com>
2014-11-03 14:02:29 +0000

    Added a missing new line

--- src/BinputServiceProvider.php
+++ src/BinputServiceProvider.php
@@ -67,6 +67,7 @@ class BinputServiceProvider extends ServiceProvider
 
             $binput = new Binput($request, $security);
             $app->refresh('request', $binput, 'setRequest');
+
             return $binput;
         });
 
